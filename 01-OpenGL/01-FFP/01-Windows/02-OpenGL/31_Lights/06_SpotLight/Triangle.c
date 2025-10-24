6dc574dc477e656370e1c626e6c75bcfc398e973
rajashree2001 <rajshreeadikane950@gmail.com>
2025-10-24 14:35:03 +0530

    added spotlight assighment

---
+++ 01-OpenGL/01-FFP/01-Windows/02-OpenGL/31_Lights/06_SpotLight/Triangle.c
@@ -0,0 +1,673 @@
+﻿// Win32 headers 
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+// OpenGL Related Header Files
+#include <gl/GL.h> // visual studio 
+
+#include <gl/GLU.h>
+
+// Custom header files
+#include "Triangle.h"
+
+// OpenGL related libraries
+#pragma comment(lib, "opengl32.lib") //give linker the link file for import
+#pragma comment(lib, "GLU32.lib")
+
+//Macros
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+//global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global variable declarations
+//variable related to Full Screen
+BOOL gbFullScreen = FALSE;
+HWND ghwnd = NULL;
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev;
+
+// Variable related with File I/O
+char gszLogFuileName[] = "Log.txt";
+FILE* gpFile = NULL;
+
+// Active window related variables
+BOOL gbActiveWindow = FALSE;
+
+// Exit key press related 
+BOOL gbEscapeKeyIsPressed = FALSE;
+
+// OpenGL related global variables
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+
+GLfloat angle = 0.0f;
+GLfloat spotCutoff = 30.0f; // Initial 30 degree cone
+// Spotlight Variables
+GLfloat lightAmbient[] = { 0.3f, 0.3f, 0.1f, 1.0f };  // Dark yellow ambient
+GLfloat lightDiffuse[] = { 0.8f, 0.8f, 0.2f, 1.0f };  // Dark yellow diffuse
+GLfloat lightSpecular[] = { 0.9f, 0.9f, 0.3f, 1.0f }; // Dark yellow specular
+GLfloat lightPosition[] = { 0.0f, 5.0f, 0.0f, 1.0f }; // Light above the plane
+GLfloat spotExponent = 2.0f; // Focus of the spotlight
+
+BOOL blight = FALSE;
+
+
+// Entry Point Function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+
+	// function declaration
+	int initialize(void);
+	void display(void);
+	void update(void);
+	void uninitialize(void);
+
+
+	//variable declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("RTR6 : My Window");
+	BOOL bDone = FALSE;
+
+	// Create Log File
+	gpFile = fopen(gszLogFuileName, "w");
+
+	if (gpFile == NULL)
+	{
+		MessageBox(NULL, TEXT("Log file creation failed!"), TEXT("File I/O Error"), MB_OK);
+		exit(0);
+	}
+	else
+	{
+		fprintf(gpFile, "File succesfully created\n");
+	}
+	// code
+	// window class initialization
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MY_ICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MY_ICON));
+
+	// Regestration of window class
+	RegisterClassEx(&wndclass);
+
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	int xPos = (screenWidth - WIN_WIDTH) / 2;
+	int yPos = (screenHeight - WIN_HEIGHT) / 2;
+
+	// Create Window	 
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("Rajashree Adikane"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		xPos,
+		yPos,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+	// Show window
+	ShowWindow(hwnd, iCmdShow);
+
+	//Paint background of the window
+	UpdateWindow(hwnd);
+
+
+	// initialize
+	int result = initialize();
+	if (result != 0)
+	{
+		fprintf(gpFile, "Initialize() : Failed!!!\n");
+		DestroyWindow(hwnd);
+		hwnd = NULL;
+	}
+	else
+	{
+		fprintf(gpFile, "initialize() : Completed successfully...\n");
+	}
+
+	// Set this window as foreground and active window
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+	// Game Loop
+	while (bDone == FALSE)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				bDone = TRUE;
+			}
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow == TRUE)
+			{
+				if (gbEscapeKeyIsPressed == TRUE)
+				{
+					bDone = TRUE;
+				}
+				// Render
+				display();
+				// update
+				update();
+			}
+		}
+	}
+
+	// uninitialize
+	uninitialize();
+	return ((int)msg.wParam);
+}
+
+// Callback function
+LRESULT CALLBACK WndProc(HWND hwnd, UINT imsg, WPARAM wParam, LPARAM lParam)
+{
+	// local function declarations
+	void toggleFullScreen(void);
+	void resize(int, int);
+	void uninitialize(void);
+	// code
+	switch (imsg)
+	{
+	case WM_CREATE:
+		ZeroMemory((void*)&wpPrev, sizeof(WINDOWPLACEMENT));
+		wpPrev.length = sizeof(WINDOWPLACEMENT);
+		break;
+	case WM_SETFOCUS:
+		gbActiveWindow = TRUE;
+		break;
+	case WM_KILLFOCUS:
+		gbActiveWindow = FALSE;
+		break;
+	case WM_ERASEBKGND:
+		return(0);
+	case WM_SIZE:
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			gbEscapeKeyIsPressed = TRUE;
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case 'F':
+		case 'f':
+			if (gbFullScreen == FALSE)
+			{
+				toggleFullScreen();
+				gbFullScreen = TRUE;
+			}
+			else
+			{
+				toggleFullScreen();
+				gbFullScreen = FALSE;
+			}
+			break;
+		case 'l':
+			blight = !blight;
+			if (blight) {
+				glEnable(GL_LIGHTING);
+				glEnable(GL_LIGHT0);
+
+				// Set the spotlight position above the center
+				lightPosition[0] = 0.0f;
+				lightPosition[1] = 5.0f;  // height above the plane
+				lightPosition[2] = 0.0f;
+
+				// Spotlight direction towards center of plane
+				GLfloat spotDirection[] = { 0.0f - lightPosition[0],
+											0.0f - lightPosition[1],
+											0.0f - lightPosition[2] };
+				glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+				glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spotDirection);
+				glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, spotCutoff);
+
+				fprintf(gpFile, "Spotlight Enabled and Focused on Center\n");
+				fflush(gpFile);
+			}
+			else {
+				glDisable(GL_LIGHT0);
+				glDisable(GL_LIGHTING);
+				fprintf(gpFile, "Lighting Disabled\n");
+				fflush(gpFile);
+			}
+			break;
+
+		case '+': // Increase spotlight angle
+		case '=':
+			if (spotCutoff < 90.0f) {
+				spotCutoff += 5.0f;
+				fprintf(gpFile, "Spotlight angle increased to: %.1f degrees\n", spotCutoff);
+				fflush(gpFile);
+			}
+			break;
+
+		case '-': // Decrease spotlight angle
+		case '_':
+			if (spotCutoff > 1.0f) {
+				spotCutoff -= 5.0f;
+				fprintf(gpFile, "Spotlight angle decreased to: %.1f degrees\n", spotCutoff);
+				fflush(gpFile);
+			}
+			break;
+
+
+		default:
+			break;
+		}
+		break;
+	case WM_CLOSE:
+		uninitialize();
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+	return (DefWindowProc(hwnd, imsg, wParam, lParam));
+}
+
+void toggleFullScreen(void)
+{
+	//variable declarations
+	MONITORINFO mi;
+	ZeroMemory((void*)&mi, sizeof(MONITORINFO));
+	mi.cbSize = sizeof(MONITORINFO);
+	//code
+	if (gbFullScreen == FALSE)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP,
+					mi.rcMonitor.left,
+					mi.rcMonitor.top,
+					mi.rcMonitor.right - mi.rcMonitor.left,
+					mi.rcMonitor.bottom - mi.rcMonitor.top,
+					SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+		}
+		ShowCursor(FALSE);
+	}
+	else
+	{
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+		ShowCursor(TRUE);
+	}
+
+}
+
+int initialize(void)
+{
+	//code
+	// function declration
+	void printGLInfo(void);
+	void resize(int, int);
+
+	// variable declarations
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex = 0;
+
+	// code
+	// PIXELFORMATDESCRIPTOR initialization
+	ZeroMemory((void*)&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+	pfd.cDepthBits = 32;
+
+	// Get DC
+	ghdc = GetDC(ghwnd);
+	if (ghdc == NULL)
+	{
+		fprintf(gpFile, "GetDC() : function failed!\n");
+		return (-1);
+	}
+
+	// Get matching pixel format index using HDC and pfd
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0)
+	{
+		fprintf(gpFile, "ChoosePixelFormat() : Failed!\n");
+		return (-2);
+	}
+
+	// Select the pixel format of found index
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+	{
+		fprintf(gpFile, "SetPixelFormat() : Failed!\n");
+		return (-3);
+	}
+
+	// Create Rendering context using HDC , PFD and Pixel Format Index and choosen pixel format index
+	ghrc = wglCreateContext(ghdc);
+	if (ghrc == NULL)
+	{
+		fprintf(gpFile, "wglCreateContext() : Failed!\n");
+		return (-4);
+	}
+
+	// Make this rendering context as current context
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE)
+	{
+		fprintf(gpFile, "wglMakeCurrent() : Failed!\n");
+		return (-5);
+	}
+
+	//printGLInfo
+	printGLInfo();
+
+	// From here Onwords OpenGL code starts
+	// Tell OpenGL to choose the color to clear the screen
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // state fuc
+
+	// Depth related code
+	glShadeModel(GL_SMOOTH); // for smoothness
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL); // glcleardepth madhlya 1.0 fragment value <=
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	// DARK YELLOW SPOTLIGHT Configuration
+	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecular);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+
+	// Spotlight specific properties
+	glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, spotCutoff);        // Initial cutoff
+	glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, spotExponent);    // Focus of spotlight
+
+	glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1.0f);
+	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05f);
+	glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.01f);
+
+	// Material properties for the plane - make it respond well to yellow light
+	GLfloat matAmbient[] = { 0.8f, 0.8f, 0.6f, 1.0f };     // Warm ambient
+	GLfloat matDiffuse[] = { 0.9f, 0.9f, 0.7f, 1.0f };     // Warm diffuse
+	GLfloat matSpecular[] = { 1.0f, 1.0f, 0.8f, 1.0f };    // Warm specular
+	GLfloat matShininess[] = { 30.0f };
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, matAmbient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, matDiffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, matSpecular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, matShininess);
+
+	// Enable color material
+	glEnable(GL_COLOR_MATERIAL);
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+
+
+
+	//if we want to add light then Normal  Line is very imp it shows the orientation of the light its the perpendicular 
+
+
+	//warm up resize
+	resize(WIN_WIDTH, WIN_HEIGHT);
+	return (0);
+}
+
+void printGLInfo(void)
+{
+	//print OpenGL info
+	fprintf(gpFile, "*********************\n");
+	fprintf(gpFile, "OpenGL Vendor  : %s\n", glGetString(GL_VENDOR));
+	fprintf(gpFile, "OpenGL Renderer: %s\n", glGetString(GL_RENDERER));
+	fprintf(gpFile, "OpenGL Version : %s\n", glGetString(GL_VERSION));
+	fprintf(gpFile, "*********************\n");
+
+}
+
+void resize(int width, int height)
+{
+	// code
+	// if height by accident is less than 0 make it 1
+	if (height < 0)
+	{
+		height = 1;
+	}
+	// set the view port
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	//set matrix projection mode
+	glMatrixMode(GL_PROJECTION);
+
+	//set identity to matrix
+	glLoadIdentity();
+
+	//Do perspective projection
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+
+}
+	
+void display()
+{
+	// Clear the color and depth buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Set model-view matrix
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Camera setup
+	gluLookAt(0.0f, 5.0f, 8.0f,   // Eye position
+		0.0f, 0.0f, 0.0f,   // Look at center
+		0.0f, 1.0f, 0.0f);  // Up vector
+
+	// Update spotlight if enabled
+	if (blight) {
+		// Spotlight position above center
+		lightPosition[0] = 0.0f;
+		lightPosition[1] = 5.0f;
+		lightPosition[2] = 0.0f;
+
+		// Direction towards center
+		GLfloat spotDirection[] = {
+			0.0f - lightPosition[0],
+			0.0f - lightPosition[1],
+			0.0f - lightPosition[2]
+		};
+
+		glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spotDirection);
+		glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, spotCutoff);
+		glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, spotExponent);
+	}
+
+	// Draw plane (XZ plane)
+	glBegin(GL_QUADS);
+	glColor3f(0.4f, 0.4f, 0.3f); // Neutral color to reflect yellow light
+	glNormal3f(0.0f, 1.0f, 0.0f);
+
+	glVertex3f(-10.0f, 0.0f, -10.0f);
+	glVertex3f(-10.0f, 0.0f, 10.0f);
+	glVertex3f(10.0f, 0.0f, 10.0f);
+	glVertex3f(10.0f, 0.0f, -10.0f);
+	glEnd();
+
+	// Draw spotlight source as small cube
+	if (blight) {
+		glPushMatrix();
+		glTranslatef(lightPosition[0], lightPosition[1], lightPosition[2]);
+		glColor3f(0.8f, 0.8f, 0.2f); // Dark yellow
+
+		float size = 0.1f;
+		glBegin(GL_QUADS);
+		// Front
+		glVertex3f(-size, -size, size);
+		glVertex3f(size, -size, size);
+		glVertex3f(size, size, size);
+		glVertex3f(-size, size, size);
+
+		// Back
+		glVertex3f(-size, -size, -size);
+		glVertex3f(-size, size, -size);
+		glVertex3f(size, size, -size);
+		glVertex3f(size, -size, -size);
+
+		// Top
+		glVertex3f(-size, size, -size);
+		glVertex3f(-size, size, size);
+		glVertex3f(size, size, size);
+		glVertex3f(size, size, -size);
+
+		// Bottom
+		glVertex3f(-size, -size, -size);
+		glVertex3f(size, -size, -size);
+		glVertex3f(size, -size, size);
+		glVertex3f(-size, -size, size);
+
+		// Right
+		glVertex3f(size, -size, -size);
+		glVertex3f(size, size, -size);
+		glVertex3f(size, size, size);
+		glVertex3f(size, -size, size);
+
+		// Left
+		glVertex3f(-size, -size, -size);
+		glVertex3f(-size, -size, size);
+		glVertex3f(-size, size, size);
+		glVertex3f(-size, size, -size);
+		glEnd();
+		glPopMatrix();
+	}
+
+	// Draw soft spotlight cone on floor
+	if (blight) {
+		float coneRadius = tanf(spotCutoff * 3.14159f / 180.0f) * lightPosition[1];
+
+		glDisable(GL_LIGHTING);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glColor4f(1.0f, 1.0f, 0.6f, 0.3f); // Soft yellow glow
+
+		glBegin(GL_TRIANGLE_FAN);
+		glVertex3f(0.0f, 0.01f, 0.0f); // center
+		for (int i = 0; i <= 50; i++) {
+			float theta = 2.0f * 3.14159f * i / 50;
+			float x = coneRadius * cosf(theta);
+			float z = coneRadius * sinf(theta);
+			glVertex3f(x, 0.01f, z);
+		}
+		glEnd();
+
+		glDisable(GL_BLEND);
+		glEnable(GL_LIGHTING);
+	}
+
+
+	SwapBuffers(ghdc);
+}
+
+
+
+void update(void)
+{
+	// code
+
+	angle = angle + 0.5f;
+	if (angle >= 360.0f)
+	{
+		angle = angle - 360.0f;
+	}
+
+
+	//angleRectangle = angleRectangle - 2.0f;
+	//if (angleRectangle <= 0)
+	//{
+	//	angleRectangle = angleRectangle + 360.0f;
+	//}
+}
+
+void uninitialize(void)
+{
+	// function declaration
+	void toggleFullsSreen(void);
+	// code
+	// user is exiting in full screen then restore full screen in normal
+	if (gbFullScreen == TRUE)
+	{
+		toggleFullScreen();
+		gbFullScreen = FALSE;
+	}
+
+	// Make hdc as current context by releasing rending context as current contex
+	if (wglGetCurrentContext() == ghrc)
+	{
+		wglMakeCurrent(NULL, NULL);
+	}
+
+	// delete the rendering context
+	if (ghrc)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+
+	// release the DC
+	if (ghdc)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+
+	if (ghwnd)
+	{
+		DestroyWindow(ghwnd);
+		ghwnd = NULL;
+	}
+	//close the file
+	if (gpFile != NULL)
+	{
+		fprintf(gpFile, "Program terminated successfully\n");
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
